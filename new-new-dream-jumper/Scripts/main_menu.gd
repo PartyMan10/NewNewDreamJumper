19dfcf033d8d313b7b65c8bec35d044837fa8876
Lyra <lyraholland2010@gmail.com>
2025-11-24 16:39:35 -0500

    init
    BIG UPDATE! Character put onto land, therei s an idle animation. Glitch with moving, turning left moves player far right but its fine just  another bug. I am currently working on a walking animation!!!

--- new-new-dream-jumper/Scripts/main_menu.gd
+++ new-new-dream-jumper/Scripts/main_menu.gd
@@ -12,12 +12,12 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://tutorial.tscn")
+	get_tree().change_scene_to_file("res://World_Map/world.tscn")
 
 
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 
-#func _on_next_pressed() -> void:
-	#get_tree().change_scene_to_file("res://world.tscn")	
+func _on_next_pressed() -> void:
+	get_tree().change_scene_to_file("res://world.tscn")	
