844cd8abba75108fded8703e1be34818f3560875
Lyra <lyraholland2010@gmail.com>
2025-11-26 21:13:13 -0500

    init
    fixing

--- new-new-dream-jumper/Scripts/2darea.gd
+++ new-new-dream-jumper/Scripts/2darea.gd
@@ -6,13 +6,13 @@ func _process(delta):
 	var direction = Vector2.ZERO
 
 	if Input.is_action_pressed("ui_right"):
-		direction.x += 1
+		direction.x += 2
 	if Input.is_action_pressed("ui_left"):
-		direction.x -= 1
+		direction.x -= 2
 	if Input.is_action_pressed("ui_down"):
-		direction.y += 1
+		direction.y += 2
 	if Input.is_action_pressed("ui_up"):
-		direction.y -= 1
+		direction.y -= 2
 
 	position += direction.normalized() * SPEED * delta
 
